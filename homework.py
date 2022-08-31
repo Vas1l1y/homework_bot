5733a4763e1563a5bc112b4990e86b00443d5ce5
Василий Ларькин <larkin-vk@mail.ru>
2022-08-31 11:22:00 +0300

    Project 6. V.1.2

--- homework.py
+++ homework.py
@@ -110,7 +110,6 @@ def check_response(response):
     return homeworks
 
 
-
 def parse_status(homework):
     """Извлекает из информации о конкретной домашке статус работы."""
     homework_name = homework.get('homework_name')
